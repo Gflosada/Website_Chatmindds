c5357ec3d302638146d275a1b85689a875f7e0a9
German <gflosadaz@gmail.com>
2024-11-01 13:05:27 -0400

    first commit

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import App from "./pages/contactPage/App"; // Assuming 'App.jsx' is the main file for Home
+import App from "./pages/homePage/App"; // Assuming 'App.jsx' is the main file for Contact
+
+const App = () => {
+  
+  return (
+   <div>
+
+    <App />
+    <App />
+
+   </div>
+  )
+}
+
+export default App
